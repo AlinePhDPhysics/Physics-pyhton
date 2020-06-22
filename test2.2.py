60e760d432d4de384c9800579300e188f60224ed
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-22 15:46:28 -0300

    If/if/else fuction

---
+++ test2.2.py
@@ -0,0 +1,9 @@
+x = 22
+y = 6
+z = 2020
+if x >=1 and x <= 31:
+    if y > 0 and y < 22:
+        if x + y >= z:
+            print ("Today's date is 6/22/2020.")
+        else:
+            print ("Today's date is not 6/22/2020.")
