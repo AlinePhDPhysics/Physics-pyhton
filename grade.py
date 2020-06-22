ec6e24889dbd8e61f3b9bd56074cffa62e263ec3
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-22 17:58:00 -0300

    grade

---
+++ grade.py
@@ -0,0 +1,11 @@
+grade = input ("Enter grade value:")
+grade = float (grade)
+
+if grade >= 5.0:
+ print ("Congratulations! You are approved!")
+if grade < 5.0:
+ print ("Recovery!")
+ print ("Need to study more!")
+ print ("And try hard!")
+
+print ("Have a nice day!")
