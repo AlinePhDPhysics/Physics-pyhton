c65276665cc6ee7c22bd19cbb4aefe13de9358d6
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-22 17:10:16 -0300

    Name/age

--- test.age.py
+++ test.age.py
@@ -2,5 +2,5 @@ name = input ("Enter your name: ")
 age = input ("Enter your age: ")
 age = int (age)
 
-print ("Hello", name,"!")
+print ("Hello " + name + "!")
 print ("You have", age, "years old!")
