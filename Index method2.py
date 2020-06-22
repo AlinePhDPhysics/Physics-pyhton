a5acaaeb45399daedd37606ee61e0b4ffb0fb15d
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-22 15:30:28 -0300

    if/else function

---
+++ Index method2.py
@@ -0,0 +1,7 @@
+#List of approved students
+students = ['Paul', 'Carl', 'Ed', 'Ken', 'Rebeka', 'Lory']
+
+print (students.index('Lory'))
+print (students.index('Ken'))
+print (students.index('Carl'))
+print ('Thanks!')
---
+++ teste2.1.py
@@ -0,0 +1,6 @@
+aluno = "João"
+disciplina = "Python"
+if aluno =="João" and disciplina =="Python":
+    print("Aprovado")
+else:
+    print("Aluno não cadastrado.")
