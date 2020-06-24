77cafadf79aada6beac32ba375161610aa9bc485
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-24 16:09:15 -0300

    break function

---
+++ break function.py
@@ -0,0 +1,13 @@
+#exemplo de uso de break
+
+a = 0
+while 1 == 1 :
+
+    print ( a )
+    if a > 3:
+        break
+    a += 1
+else:
+    print ( "Resultado do teste deu falso " )
+
+print ( "Tenha um bom dia! " )
