87ae56280a63f2f08eda8a069d33cbae5396213e
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-24 15:56:30 -0300

    for function 2

---
+++ for function2.py
@@ -0,0 +1,10 @@
+#exemplo de uso de for
+#programa que imprime na tela os primeiros n números naturais
+
+n = input ( "Digite o valor de n: " )
+n = int (n)
+
+for contador in range ( 1 , n+1, 1) :
+    print ( contador )
+
+print ( "Tenha um bom dia ! " )
