8aeb2eceb7ef07e01ef4da74d86bffc4de21096a
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-25 19:37:29 -0300

    operações aritméticas

---
+++ test4.py
@@ -0,0 +1,11 @@
+a = float(input("Digite o primeiro número:"))
+b = float(input("Digite o segundo número:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+print('Muito bom!')
