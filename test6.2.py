bb2b4cf5f95bdfd1073e66605b3ad2442b74de7d
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-27 17:34:47 -0300

    test6.2

---
+++ test6.2.py
@@ -0,0 +1,9 @@
+print("Second Number Pattern ")
+lastNumber = 6
+for row in range(1, lastNumber):
+    for column in range(1, row + 1):
+        print(column, end=' ')
+    print("")
+    for column in range(1, row + 1):
+        print(column, end=' ')
+    print("")
