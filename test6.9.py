8236402116c3dc3de045e242f7bfa2999884990e
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-06-28 23:38:43 -0300

    test6.9

--- test6.9.py
+++ test6.9.py
@@ -7,10 +7,13 @@ b = float(b)
 imc = (a/(b**2))
 imc = float (imc)
 print ('O seu IMC é:'"%.2f" % imc)
-if imc>30.0:
-    print ('Você está obeso!Precisa de dieta e atividades físicas!')
-else: 
-    if imc<30.0:
-         print ("Parabéns! Você está no peso ideal!")
-
-
+if imc <= 18.5:
+    print ('magreza')
+if imc >= 18.5 and imc <= 24.9: 
+    print ('normal') 
+if imc>=25.0 and imc<=29.9: 
+    print ('sobrepeso') 
+else:
+    if imc>30.0:
+        print ('Você está obeso!Precisa de dieta e atividades físicas!')
+print ('Obrigado por realizar o teste!')
